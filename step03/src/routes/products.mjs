a61ba99e76c38678214cdf0e4ff4bcb2373384dd
quemet <129742514+quemet@users.noreply.github.com>
2024-02-02 08:12:15 +0100

    Update products.mjs

--- step03/src/routes/products.mjs
+++ step03/src/routes/products.mjs
@@ -14,6 +14,13 @@ productRouter.get("/:id", (req, res) => {
   const product = products.find((product) => product.id == producId);
   const message = `Le produit dont l'id vaut ${productId} a bien été récuperé`;
   res.json(sucess(message, product));
+});
+
+productRouter.post("/", (req, res) => {
+  const id = getUniqueId(products);
+  const createdProduct = { ...req.body, ...{ id: id, created: new Date() } };
+  products.push(createdProduct);
+  const message = `Le produit ${createdProduct.name} a bien été crée`;
 })
 
 export { productRouter };
